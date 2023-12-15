f3f6c3e63c8c930589d6fbdfe6285a78c09cd1f4
ramdio12 <luib.diomar@gmail.com>
2023-12-15 17:15:57 +0800

    duration adjusted for Homecard

--- src/Components/HomeCard.jsx
+++ src/Components/HomeCard.jsx
@@ -5,8 +5,8 @@ const HomeCard = ({ id, slogan, text, image }) => {
     console.log(slogan)
     return (
         <motion.div
-            initial={{ opacity: 0, translateY: 60 }}
-            whileInView={{ opacity: 1, translateY: 0 }}
+            initial={{ translateY: 60, opacity: 0 }}
+            whileInView={{ translateY: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: id * 0.7 }}
             viewport={{ once: true }}
 
