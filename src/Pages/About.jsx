d1a9a73906aae8aecbd1a05939d026ce98546d01
ramdio12 <luib.diomar@gmail.com>
2023-12-15 21:01:24 +0800

    magrin-top adjustment on about section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <section id="about" className="w-full text-center md:flex items-center justify-center font-mono min-h-screen">
             <div className="basis-1/2 px-12">
-                <h2 className=" text-4xl md:text-6xl 2xl:text-7xl pb-4">About Us</h2>
+                <h2 className=" text-4xl md:text-6xl 2xl:text-7xl pb-4 mt-20 md:mt-0">About Us</h2>
                 <p className=" text-lg md:text-xl 2xl:text-2xl text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nunc nulla, tristique quis varius nec, sagittis nec erat. Etiam rhoncus velit elementum, convallis lorem vel, viverra ante. Praesent sit amet odio gravida, cursus tortor quis, maximus mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In consectetur orci eu orci tincidunt tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur at iaculis velit, ac tempor metus. Pellentesque dictum eget leo id tempus. Donec venenatis urna eget odio euismod cursus. </p>
             </div>
             <div className="flex items-center justify-center basis-1/2">
